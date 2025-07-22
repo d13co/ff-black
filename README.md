144d074bae85015ccc1062bf170f9ad8cdb30761
Bit D13 <bit@d13.co>
2025-07-23 01:32:15 +0300

    README, docs

--- README.md
+++ README.md
@@ -2,17 +2,26 @@
 
 ## Requirements
 
-- Payment to REVENUE_ADDRESS
-- Purchase options:
-  - In ALGO with ALGO_PRICE
-  - In PURCHASE_ASSET with ASSET_AMOUNT
+[v] Payment to REVENUE_ADDRESS
+[v] Purchase options:
+  [v] In ALGO with ALGO_PRICE
+  [v] In PURCHASE_ASSET with ASSET_AMOUNT
 
-- Sales are frozen until UNFREEZE_TIME
-- After UNFREEZE_TIME anyone will be able to unfreeze their account
+[v] Sales are frozen until UNFREEZE_TIME
+[v] After UNFREEZE_TIME anyone will be able to unfreeze their account
 
-- Only to whitelisted addresses
-  - Allowed amount to mint: 5
+[v] Only to whitelisted addresses
+  [v] Allowed amount to mint: 5
 
-- Admin sets OPTIONS / WL
-- Admin can opt in to assets
+[v] Admin sets OPTIONS / WL
+[v] Admin can opt in to assets
 
+## Setup
+
+- Deploy contract
+- Change your asset's freeze account to be the contract's escrow account (so it can freeze/unfreeze)
+- Choose a revenue address to receive payments. Opt it in to USDC.
+- Configure the contract with `setConfig`. Also opts in to sellingAsset.
+  - Fields are documented at the top of contract.algo.ts
+- Add whitelisted accounts with `addWhitelist`. You can do 8x per app call, 128 per group txn.
+- Send selling asset to contract.
--- projects/ff-black/smart_contracts/ff_black/contract.algo.ts
+++ projects/ff-black/smart_contracts/ff_black/contract.algo.ts
@@ -14,8 +14,9 @@ import {
   Txn,
   uint64,
 } from '@algorandfoundation/algorand-typescript'
-import { abimethod, Address, Struct, UintN64 } from '@algorandfoundation/algorand-typescript/arc4'
+import { Address, Struct, UintN64 } from '@algorandfoundation/algorand-typescript/arc4'
 
+// see global state for docs
 class ContractConfig extends Struct<{
   revenueAddress: Address
   sellingAsset: UintN64
@@ -26,18 +27,28 @@ class ContractConfig extends Struct<{
 }> {}
 
 export class FfBlack extends Contract {
+  // configures contract stuff
   admin = GlobalState<Account>({ initialValue: Txn.sender })
+  // receiver of payments
   revenueAddress = GlobalState<Account>({ initialValue: Txn.sender })
-
+  // asset ID we are selling
   sellingAsset = GlobalState<Asset>({ initialValue: Asset(0) })
+  // price of asset in ALGO
   algoPrice = GlobalState<uint64>({ initialValue: 0 })
+  // alternative purchase asset, e.g. USDC
   purchaseAsset = GlobalState<Asset>({ initialValue: Asset(0) })
+  // price in alternative purchase asset
   assetPrice = GlobalState<uint64>({ initialValue: 0 })
-
+  // unix timestamp after which unfreezing becomes open to anyone
+  // NOTE: Second-level precision! e.g. 1753223497 good / 1753223497000 bad
   unfreezeTime = GlobalState<uint64>({ initialValue: 0 })
-
+  // whitelist box map, value: remaining quota, e.g. 5
   wl = BoxMap<Account, uint64>({ keyPrefix: '' })
 
+  /**
+   * Buy NFT. Account must be on whitelist. Freezes buyer after, if not frozen already
+   * @param txn Payment or asset transfer transaction
+   */
   public buy(txn: gtxn.Transaction) {
     const buyer = Txn.sender
     const sellingAsset = this.sellingAsset.value
@@ -46,9 +57,14 @@ export class FfBlack extends Contract {
 
     // ensure WL exists and has quota. save updated quota
     ensure(this.wl(buyer).exists, 'ERR:NO_WL')
-    const remaining = this.wl(buyer).value
-    ensure(remaining >= qty, 'ERR:QUOTA')
-    this.wl(buyer).value = remaining - qty
+    const quota = this.wl(buyer).value
+    ensure(quota >= qty, 'ERR:QUOTA')
+    const remaining: uint64 = quota - qty
+    if (remaining === 0) {
+      this.wl(buyer).delete()
+    } else {
+      this.wl(buyer).value = quota - qty
+    }
 
     // send NFT(s)
     itxn
@@ -59,8 +75,8 @@ export class FfBlack extends Contract {
       })
       .submit()
 
-    // freeze buyer if needed
-    if (!sellingAsset.frozen(buyer)) {
+    // freeze buyer if needed. only until unfreezeTime
+    if (Global.latestTimestamp < this.unfreezeTime.value && !sellingAsset.frozen(buyer)) {
       itxn
         .assetFreeze({
           freezeAsset: sellingAsset,
@@ -77,6 +93,7 @@ export class FfBlack extends Contract {
    * @returns quantity of NFTs to buy
    */
   private validatePayment(txn: gtxn.Transaction): uint64 {
+    // if no payment txn, fail gracefully before accessing txn fields
     ensure(Txn.groupIndex > 0, 'ERR:GTXN')
     switch (txn.type) {
       case TransactionType.Payment:
@@ -105,20 +122,31 @@ export class FfBlack extends Contract {
     }
   }
 
+  /**
+   * Unfreeze account. Before unfreezeTime, only admin can unfreeze
+   * After unfreezeTime, anyone can unfreeze anyone else
+   * @param account account to unfreeze
+   */
   public unfreeze(account: Account) {
+    // admin only can unfreeze before unfreezeTime
+    // open after that
     if (Global.latestTimestamp < this.unfreezeTime.value) {
-      // admin only can unfreeze
+      this.ensureAdmin()
     }
-    const sellingAsset = this.sellingAsset.value
     itxn
       .assetFreeze({
-        freezeAsset: sellingAsset,
+        freezeAsset: this.sellingAsset.value,
         freezeAccount: account,
         frozen: false,
       })
       .submit()
   }
 
+  /**
+   * Add multiple accounts to whitelist.
+   * Buy quota is hardcoded to 5
+   * @param accounts accounts to add to WL
+   */
   public addWhitelist(accounts: arc4.Address[]) {
     this.ensureAdmin()
 
@@ -127,6 +155,10 @@ export class FfBlack extends Contract {
     }
   }
 
+  /**
+   * Remove accounts from whitelist. Fails if any are not on WL
+   * @param accounts accounts to remove from WL
+   */
   public removeWhitelist(accounts: arc4.Address[]) {
     this.ensureAdmin()
 
@@ -135,9 +167,18 @@ export class FfBlack extends Contract {
     }
   }
 
+  /**
+   * Opt in to asset. Should not be needed
+   * @param asset asset to opt in to
+   */
   public optin(asset: Asset) {
     this.ensureAdmin()
 
+    this._optin(asset)
+  }
+
+  // Subroutine reused by public optin and setConfig
+  private _optin(asset: Asset) {
     itxn
       .assetTransfer({
         xferAsset: asset,
@@ -146,18 +187,10 @@ export class FfBlack extends Contract {
       .submit()
   }
 
-  @abimethod({ readonly: true })
-  public getConfig(): ContractConfig {
-    return new ContractConfig({
-      revenueAddress: new Address(this.revenueAddress.value),
-      sellingAsset: au64(this.sellingAsset.value.id),
-      algoPrice: au64(this.algoPrice.value),
-      purchaseAsset: au64(this.purchaseAsset.value.id),
-      assetPrice: au64(this.assetPrice.value),
-      unfreezeTime: au64(this.unfreezeTime.value),
-    })
-  }
-
+  /**
+   * Set configuration values & opt in to selling asset - WARNING: REPLACES ALL VALUES
+   * @param config new config
+   */
   public setConfig(config: ContractConfig) {
     this.ensureAdmin()
 
@@ -167,17 +200,57 @@ export class FfBlack extends Contract {
     this.purchaseAsset.value = Asset(config.purchaseAsset.native)
     this.assetPrice.value = config.assetPrice.native
     this.unfreezeTime.value = config.unfreezeTime.native
+
+    this._optin(this.sellingAsset.value)
   }
 
+  /**
+   * change admin account. Admin only
+   * @param newAdmin new admin account
+   */
+  public changeAdmin(newAdmin: Account) {
+    this.ensureAdmin()
+
+    this.admin.value = newAdmin
+  }
+
+  /**
+   * Close out of an asset, sending everything to caller (admin)
+   * @param asset asset to close out of
+   */
+  public assetCloseout(asset: Asset) {
+    this.ensureAdmin()
+
+    itxn
+      .assetTransfer({
+        xferAsset: asset,
+        assetCloseTo: Txn.sender,
+      })
+      .submit()
+  }
+
+  /**
+   * Close out of all algo, sending everything to caller (admin)
+   * Requires baseline MBR, i.e. no boxes remaining
+   * @param asset asset to close out of
+   */
+  public algoCloseout() {
+    this.ensureAdmin()
+
+    itxn
+      .payment({
+        closeRemainderTo: Txn.sender,
+      })
+      .submit()
+  }
+
+  // ensure caller is admin
   private ensureAdmin() {
     ensure(Txn.sender === this.admin.value, 'ERR:UNAUTH')
   }
 }
 
-function au64(value: uint64) {
-  return new UintN64(value)
-}
-
+// like assert but with arc56 logging
 function ensure(cond: boolean, message: string) {
   if (!cond) {
     log(message)
